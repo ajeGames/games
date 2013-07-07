4418e868d8544caecb44d94995398cfcd134b7fa
Dave Mount <davemount@gmail.com>
2013-07-07 11:07:36 -0700

    Added logging to spinner configuration.

--- src/main/java/com/ajegames/picnic/service/PicnicSpinnerFactory.java
+++ src/main/java/com/ajegames/picnic/service/PicnicSpinnerFactory.java
@@ -2,6 +2,8 @@ package com.ajegames.picnic.service;
 
 import com.ajegames.picnic.*;
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -10,48 +12,47 @@ import java.util.List;
  */
 public class PicnicSpinnerFactory {
 
+  private static Logger LOG = LoggerFactory.getLogger(PicnicSpinnerFactory.class);
+
   synchronized public static void configureSpinner(SpinnerConfiguration config) {
+    LOG.info("Configuring spinner options...");
+
     Item toAdd;
     List<Item> spinnerOptions = Lists.newArrayList();
-
     for (SpinnerItemConfig item : config.getFoods()) {
       toAdd = Item.createFood(item.getKey(), item.getDescription());
       addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
-
     for (SpinnerItemConfig item : config.getDrinks()) {
       toAdd = Item.createDrink(item.getKey(), item.getDescription());
       addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
-
     for (SpinnerItemConfig item : config.getSupplies()) {
       toAdd = Item.createSupply(item.getKey(), item.getDescription());
       addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
-
     for (SpinnerItemConfig item : config.getNuisances()) {
       if ("reducesFood".equals(item.getImpact())) {
         toAdd = Nuisance.createAgainstFood(item.getKey(), item.getDescription());
       } else if ("wipeout".equals(item.getImpact())) {
         toAdd = Nuisance.createWipeOut(item.getKey(), item.getDescription());
       } else {
-        // TODO log some kind of config error
+        LOG.warn("Problem with spinner configuration: impact '" + item.getImpact() + "' is not recognized.");
         continue;
       }
       addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
-
     for (SpinnerItemConfig item : config.getPreventions()) {
       Item counteracts = ItemCatalog.getInstance().getItem(item.getCounteracts());
-      if (counteracts != null && counteracts instanceof Nuisance) {
-        toAdd = Prevention.createPrevention(item.getKey(), item.getDescription(), (Nuisance) counteracts);
+      if (counteracts == null) {
+        LOG.warn("Problem with spinner configuration: item '" + item.getCounteracts() + "' was not found.");
+      } else if (!(counteracts instanceof Nuisance)) {
+        LOG.warn("Problem with spinner configuration: '" + item.getCounteracts() + "' is not a nuisance and cannot be counteracted.");
       } else {
-        // TODO log some kind of config error
-        continue;
+        toAdd = Prevention.createPrevention(item.getKey(), item.getDescription(), (Nuisance) counteracts);
+        addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
       }
-      addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
-
     PicnicSpinner.configureSpinner(spinnerOptions);
   }
 
