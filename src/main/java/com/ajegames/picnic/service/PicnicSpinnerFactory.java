d505f10482b92a5212279eb80fc1bc9c648c2bb0
Dave Mount <davemount@gmail.com>
2013-07-07 10:49:50 -0700

    Loads item catalog now.

--- src/main/java/com/ajegames/picnic/service/PicnicSpinnerFactory.java
+++ src/main/java/com/ajegames/picnic/service/PicnicSpinnerFactory.java
@@ -11,41 +11,54 @@ import java.util.List;
 public class PicnicSpinnerFactory {
 
   synchronized public static void configureSpinner(SpinnerConfiguration config) {
-    List<Item> items = Lists.newArrayList();
+    Item toAdd;
+    List<Item> spinnerOptions = Lists.newArrayList();
+
     for (SpinnerItemConfig item : config.getFoods()) {
-      for (int i = 0; i < item.getWeight(); i++) {
-        items.add(Item.createFood(item.getKey(), item.getDescription()));
-      }
+      toAdd = Item.createFood(item.getKey(), item.getDescription());
+      addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
+
     for (SpinnerItemConfig item : config.getDrinks()) {
-      for (int i = 0; i < item.getWeight(); i++) {
-        items.add(Item.createDrink(item.getKey(), item.getDescription()));
-      }
+      toAdd = Item.createDrink(item.getKey(), item.getDescription());
+      addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
+
     for (SpinnerItemConfig item : config.getSupplies()) {
-      for (int i = 0; i < item.getWeight(); i++) {
-        items.add(Item.createSupply(item.getKey(), item.getDescription()));
-      }
+      toAdd = Item.createSupply(item.getKey(), item.getDescription());
+      addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
+
     for (SpinnerItemConfig item : config.getNuisances()) {
-      for (int i = 0; i < item.getWeight(); i++) {
-        if ("reducesFood".equals(item.getImpact())) {
-          items.add(Nuisance.createAgainstFood(item.getKey(), item.getDescription()));
-        } else if ("wipeout".equals(item.getImpact())) {
-          items.add(Nuisance.createWipeOut(item.getKey(), item.getDescription()));
-        } else {
-          // add message to log that nuisance unsupported
-        }
+      if ("reducesFood".equals(item.getImpact())) {
+        toAdd = Nuisance.createAgainstFood(item.getKey(), item.getDescription());
+      } else if ("wipeout".equals(item.getImpact())) {
+        toAdd = Nuisance.createWipeOut(item.getKey(), item.getDescription());
+      } else {
+        // TODO log some kind of config error
+        continue;
       }
+      addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
+
     for (SpinnerItemConfig item : config.getPreventions()) {
-      for (int i = 0; i < item.getWeight(); i++) {
-        Item counteracts = ItemCatalog.getInstance().getItem(item.getCounteracts());
-        if (counteracts instanceof Nuisance) {
-          items.add(Prevention.createPrevention(item.getKey(), item.getDescription(), (Nuisance) counteracts));
-        }
+      Item counteracts = ItemCatalog.getInstance().getItem(item.getCounteracts());
+      if (counteracts != null && counteracts instanceof Nuisance) {
+        toAdd = Prevention.createPrevention(item.getKey(), item.getDescription(), (Nuisance) counteracts);
+      } else {
+        // TODO log some kind of config error
+        continue;
       }
+      addToCatalogAndSpinnerWithWeight(spinnerOptions, toAdd, item.getWeight());
     }
-    PicnicSpinner.configureSpinner(items);
+
+    PicnicSpinner.configureSpinner(spinnerOptions);
+  }
+
+  private static void addToCatalogAndSpinnerWithWeight(List<Item> spinnerOptions, Item toAdd, int weight) {
+    ItemCatalog.getInstance().addItem(toAdd);
+    for (int i = 0; i < weight; i++) {
+      spinnerOptions.add(toAdd);
+    }
   }
 }
